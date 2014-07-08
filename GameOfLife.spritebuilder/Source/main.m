c294078b54366307f276f23bc909a2eef9aefa72
manasanikil <srikantlakshminarayan@srikants-mbp.home>
2014-07-07 17:59:36 -0700

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/main.m
+++ GameOfLife.spritebuilder/Source/main.m
@@ -8,10 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-int main(int argc, char *argv[]) {
+//int main(int argc, char *argv[]) {
     
-    @autoreleasepool {
-        int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
-        return retVal;
-    }
-}
+    //@autoreleasepool {
+        //int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+        //return retVal;
+   // }
+//}
